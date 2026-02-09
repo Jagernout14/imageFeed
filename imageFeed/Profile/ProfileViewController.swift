ee784973a026be899732130ec6c903eb87ab37b2
Jagernout14 <jagernout@icloud.com>
2026-02-09 18:37:54 +0300

    feat(Profile): Добавить экран профиля Добавлен экран профиля

---
+++ imageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet weak private var avatarImageView: UIImageView!
+    @IBOutlet weak private var usernameLabel: UILabel!
+    @IBOutlet weak private var descriptionLabel: UILabel!
+    @IBOutlet weak private var loginNameLabel: UILabel!
+    
+    @IBOutlet weak private var logoutButton: UIButton!
+    
+    @IBAction private func didTapLogoutButton(_ sender: UIButton) {
+    }
+}

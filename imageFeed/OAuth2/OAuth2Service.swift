ec4b3a78c7b6e286cc7d304d0d12a278f135838b
Jagernout14 <jagernout@icloud.com>
2026-03-24 09:59:44 +0300

    Sprint 10 (#3)
    * feat(ImageFeed): Добавить функционал авторизации
    
    Добавлен экран с возможностью авторизации при помощи webView
    
    * fix(ImageFeed): Доработать авторизацию в приложении
    
    Доработана авторизация
    Добавлен SplashScreen
    Сохранение токена для избавления от постоянной авторизации при входе
    
    * style(OauthResponseBody): Исправить опечатки
    
    Исправлены опечатки внутри класса

---
+++ imageFeed/OAuth2/OAuth2Service.swift
@@ -0,0 +1,88 @@
+import Foundation
+
+final class OAuth2Service {
+    
+    // MARK: - Public Properties
+    static let shared = OAuth2Service()
+    
+    // MARK: - Private Properties
+    private let tokenStorage = OAuth2TokenStorage()
+    
+    // MARK: - Initializers
+    private init() {}
+    
+    // MARK: - Public Methods
+    func fetchOAuthToken(code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let request = makeOAuthTokenRequest(code: code) else {
+            let error = NetworkError.invalidRequest
+            print("Не удалось создать запрос на токен")
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        let task = URLSession.shared.data(for: request) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = JSONDecoder()
+                    let tokenResponse = try decoder.decode(OAuthTokenResponseBody.self, from: data)
+                    self.tokenStorage.token = tokenResponse.accessToken
+                    print("Получен и сохранен токен \(tokenResponse.accessToken)")
+                    DispatchQueue.main.async {
+                        completion(.success(tokenResponse.accessToken))
+                    }
+                } catch {
+                    print("Ошибка декодирования ответа \(error)")
+                    DispatchQueue.main.async {
+                        completion(.failure(NetworkError.decodingError(error)))
+                    }
+                }
+                
+            case .failure(let error):
+                switch error {
+                case NetworkError.httpStatusCode(let statusCode):
+                    print("НТТР ошибка от Unsplash \(statusCode)")
+                case NetworkError.urlRequestError(let urlError):
+                    print("Ошибка сети \(urlError)")
+                case NetworkError.urlSessionError:
+                    print("Ошибка сессии URLSession")
+                default:
+                    print("Неизвестная ошибка")
+                }
+                
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+    
+    // MARK: - Private Methods
+    private func makeOAuthTokenRequest(code: String) -> URLRequest? {
+        guard var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token") else {
+            return nil
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "client_secret", value: Constants.secretKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let authTokenUrl = urlComponents.url else {
+            return nil
+        }
+        
+        var request = URLRequest(url: authTokenUrl)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        return request
+    }
+}
--- imageFeed/SingleImageView/SingleImageViewController.swift
+++ imageFeed/SingleImageView/SingleImageViewController.swift
@@ -79,4 +79,3 @@ extension SingleImageViewController: UIScrollViewDelegate {
         return imageView
     }
 }
-
